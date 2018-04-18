1d690eb7014ecdb1371a692dfb7a0558578c7a85
Spark-Xl <sparkshenxl@gmail.com>
2018-04-18 16:25:30 -0400

    implement timer

---
+++ YADL Reference App/DateChecker.swift
@@ -0,0 +1,82 @@
+//
+//  DateChecker.swift
+//  YADL Reference App
+//
+//  Created by shenxialin on 18/4/2018.
+//  Copyright © 2018 Christina Tsangouri. All rights reserved.
+//
+
+import Foundation
+
+enum Group {
+    case Experimental_A
+    case Experimental_B
+    case Control
+}
+
+enum UserDefaultsKey: String {
+    case FIRSTRUNTIME
+}
+
+//let baseline_test_duration = 3
+let transform_date_month = 4
+let transform_date_day = 29
+let group_type: Group = .Experimental_A
+
+class DateChecker {
+    static let sharedDateChecker = DateChecker()
+    private let defaults = UserDefaults.standard
+    private let calendar = NSCalendar.current
+    
+//    private func shouldTransform() -> Bool {
+//        if let firstRunTime = getFirstRunTime() {
+//            let currentTime = Date()
+//            let dateA = calendar.component(.day, from: firstRunTime)
+//            let dateB = calendar.component(.day, from: currentTime)
+//            if dateB - dateA == baseline_test_duration {
+//                return true
+//            }
+//        }
+//        return false
+//    }
+    
+    private func getDayAndMonth() -> (Int, Int) {
+        let currentTime = Date()
+        let day = calendar.component(.day, from: currentTime)
+        let month = calendar.component(.month, from: currentTime)
+        return (day, month)
+    }
+    
+    private func shouldTransform() -> Bool {
+        let current = getDayAndMonth()
+        return (current.0 >= transform_date_day && current.1 >= transform_date_month)
+    }
+    
+    func groupType() -> Group {
+        if shouldTransform() {
+            return group_type
+        }
+        return .Control
+    }
+    
+    func shouldRunSurvey() -> Bool {
+        let current = getDayAndMonth()
+        let key = "key_\(100*current.1 + current.0)"
+        let finished = defaults.bool(forKey: key)
+        return !finished
+    }
+    
+    func surveyDone() {
+        let current = getDayAndMonth()
+        let key = "key_\(100*current.1 + current.0)"
+        defaults.set(true, forKey: key)
+    }
+    
+//    private func getFirstRunTime() -> Date? {
+//        if let firstRunTime = defaults.object(forKey: UserDefaultsKey.FIRSTRUNTIME.rawValue) as? Date {
+//            return firstRunTime
+//        }
+//
+//        return nil
+//    }
+}
--- YADL Reference App/MainViewController.swift
+++ YADL Reference App/MainViewController.swift
@@ -21,60 +21,31 @@ class MainViewController: UIViewController{
     var fullAssessmentItem: RSAFScheduleItem!
     var spotAssessmentItem: RSAFScheduleItem!
     var pamAssessmentItem: RSAFScheduleItem!
+    
     let fileUploader = FileUploader.sharedUploader
+    let dateChecker = DateChecker.sharedDateChecker
     
     @IBOutlet
     var tableView: UITableView!
     
     override func viewDidLoad() {
-        
         super.viewDidLoad()
-        
         self.store = RSStore()
-        self.store.set(value: true as NSSecureCoding, key: "runA")
     }
     
     override func viewDidAppear(_ animated: Bool) {
-//        let shouldDoSpot = self.store.get(key: "shouldDoSpot") as! Bool
-//
-//        if (shouldDoSpot) {
-//
-//            self.launchSpotAssessment()
-//        }
-//
-//        self.shouldDoFullAssessment()
-//        fileUploader.uploadJson(json: ["hello": "123", "what": "testing"])
-        let runA = self.store.get(key: "runA") as! Bool
-        
-        if runA {
-            self.launchFoodSurveyAssessment(type: "A")
-            self.store.set(value: false as NSSecureCoding, key: "runA")
-        } else {
-            self.launchFoodSurveyAssessment(type: "B")
-            self.store.set(value: true as NSSecureCoding, key: "runA")
-        }
+        launchSurveyIfNecessary()
     }
     
-    func shouldDoFullAssessment () {
-        
-        let currentDate = Date()
-        
-        // Implement should do full assessment
-        let fullDate = self.store.valueInState(forKey: "dateFull")
-        
-        let calendar = NSCalendar.current
-        let components = NSDateComponents()
-        components.day = 28
-        
-        
-        if(fullDate != nil){
-            let futureDate = calendar.date(byAdding: components as DateComponents, to: fullDate as! Date)
-            
-            if futureDate! <= currentDate {
-                
-                self.launchFullAssessment()
-                
-            }
+    func launchSurveyIfNecessary() {
+        if !dateChecker.shouldRunSurvey() {
+            return
+        }
+        switch dateChecker.groupType() {
+        case .Control:
+            self.launchFoodSurveyAssessment(type: "B")
+        default:
+            self.launchFoodSurveyAssessment(type: "A")
         }
     }
     
@@ -104,88 +75,24 @@ class MainViewController: UIViewController{
         
         let taskFinishedHandler: ((ORKTaskViewController, ORKTaskViewControllerFinishReason, Error?) -> ()) = { [weak self] (taskViewController, reason, error) in
             //when finised, if task was successful (e.g., wasn't canceled)
-            //process results
             
             if reason == ORKTaskViewControllerFinishReason.discarded {
                 self?.store.setValueInState(value: false as NSSecureCoding, forKey: "shouldDoSpot")
             }
             
             if reason == ORKTaskViewControllerFinishReason.completed {
+                self?.dateChecker.surveyDone()
+                
                 let taskResult = taskViewController.result
-                //appDelegate.resultsProcessor.processResult(taskResult: taskResult, resultTransforms: item.resultTransforms)
-                
-                if(item.identifier == "yadl_spot") {
-                    self?.store.set(value: false as NSSecureCoding, key: "shouldDoSpot")
-                    self?.store.setValueInState(value: true as NSSecureCoding, forKey: "spotFileExists")
-
+                do {
+                    let jsonResult = try ORKESerializer.jsonObject(for: taskResult)
+                    self?.fileUploader.uploadJson(json: jsonResult)
                 }
-                
-                if(item.identifier == "food_survey_A"){
-                    
-                    // save date that full assessment was completed
-                    
-                    let date = Date()
-                    
-//                    self?.store.setValueInState(value: date as NSSecureCoding, forKey: "fullDate")
-//                    self?.store.setValueInState(value: true as NSSecureCoding, forKey: "fullFileExists")
-                    
-                    // save for spot assessment
-                    
-//                    if let difficultActivities: [String]? = taskResult.results?.flatMap({ (stepResult) in
-//                        if let stepResult = stepResult as? ORKStepResult,
-//                            stepResult.identifier.hasPrefix("yadl_full."),
-//                            let choiceResult = stepResult.firstResult as? ORKChoiceQuestionResult,
-//                            let answer = choiceResult.choiceAnswers?.first as? String,
-//                            answer == "hard" || answer == "moderate"
-//                        {
-//                            var tempResult = stepResult.identifier
-//                            let index = tempResult.index(tempResult.startIndex, offsetBy: 10)
-//                            tempResult = tempResult.substring(from:index)
-//
-//
-//                            NSLog(tempResult)
-//
-//                            return tempResult.replacingOccurrences(of: "yadl_full.", with: "")
-//
-//                        }
-//                        return nil
-//                    }) {
-//                        if let answers = difficultActivities {
-//                            self?.store.setValueInState(value: answers as NSSecureCoding, forKey: "activity_identifiers")
-//
-//                            // save when completed full assessment
-//
-//
-//                        }
-//                    }
-                    
-//                    if let answers: [String]? = taskResult.results?.flatMap({ (stepResult) in
-//                        if let stepResult = stepResult as? ORKStepResult {
-//
-//                            return ""
-//                        }
-//                    }) {
-//
-//                    }
-                    do {
-                        let jsonResult = try ORKESerializer.jsonObject(for: taskResult)
-                        self?.fileUploader.uploadJson(json: jsonResult)
-                    }
-                    catch {
-                        print(error)
-                    }
+                catch {
+                    print(error)
                 }
-                
             }
-            
-            self?.dismiss(animated: true, completion: {
-                
-                if(item.identifier == "yadl_spot"){
-                    self!.pamAssessmentItem = AppDelegate.loadScheduleItem(filename:"pam")
-                    self?.launchActivity(forItem: (self?.pamAssessmentItem)!)
-                }
-                
-            })
+            self?.dismiss(animated: true)
         }
         
         let tvc = RSAFTaskViewController(
@@ -193,13 +100,6 @@ class MainViewController: UIViewController{
             task: task,
             taskFinishedHandler: taskFinishedHandler
         )
-        
         self.present(tvc, animated: true, completion: nil)
-        
     }
-    
-
-
-
-
 }
